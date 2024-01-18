ca354ace9c770f9912f260b0c485d802c60ec560
Jose Carlos <josecarlos.avilaperea@gmail.com>
2024-01-17 22:43:20 -0500

    feat: GifGrid added

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,8 +1,29 @@
 
+import PropTypes from 'prop-types';
+import { fetchGetGifs } from '../helpers/getGifs';
+import { useEffect } from 'react';
+
+
+
+
 export const GifGrid = ({ category }) => {
+
+    useEffect(() => {
+        fetchGetGifs(category);
+    }, [category]);
+
     return (
         <>
             <h3>{category}</h3>
         </>
     )
 }
+
+
+GifGrid.propTypes = {
+    category: PropTypes.string
+};
+
+GifGrid.defaultProps = {
+    category: 'Fata la propiedad category'
+};
