6ad53c80927eda39127b48a8bb50fb8fcae04b3a
Jose Carlos <josecarlos.avilaperea@gmail.com>
2024-01-17 20:51:43 -0500

    Added tittle, form input and gif list

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  const onAddCategory = () => {
+    setCategories([...categories, ['Dead Note']])
+  }
+
+
   return (
-    <div>GifExpertApp</div>
+    <>
+      {/* Titulo */}
+      <div>GifExpertApp</div>
+
+      {/* Input */}
+      <AddCategory />
+
+
+      {/* Listado de Gif */}
+      <button onClick={onAddCategory}>Agregar </button>
+      <ol>
+        {
+          categories.map((category, key) => {
+            return <li key={key}>{category}</li>
+
+          })
+        }
+      </ol>
+    </>
   )
 }
